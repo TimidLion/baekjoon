9bca934caa6a5a69a8426c7846d0a7219978a0b8
TimidLion <sgm02186@naver.com>
2016-09-04 17:54:28 +0900

    prime_array_maker added

---
+++ 1005_ACM_Craft.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int T;
+	scanf("%d",&T);
+	for(;T>0;T--)
+	{
+		int N,K;
+		scanf("%d %d",&N,&K);
+		int* buildTime = new int[N];
+		for(int i=0;i<N;i++)
+			scanf("%d",&buildTime[i]);
+		
+	}
+}
+
+
---
+++ prime_array_maker.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+	int range = 1000;
+	int* array = new int[range+1];
+	for(int i =2; i < range+1; i++)
+	{
+		array[i] = i;
+	}
+
+
+	for(int f = 2; f <= range; f++)
+	{
+		if(array[f] == 0)
+			continue;
+		for(int j = 2; f*j <=range; j++)
+			array[f*j] = 0;
+	}
+
+	printf("[ ");
+	for(int p = 2; p <= range; p++)
+		if( array[p] != 0)
+			printf("%d, ",array[p]);
+	printf("]\n");
+}
