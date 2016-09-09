6154cd48ee0031dc1d83f792f045cae21915204d
TimidLion <sgm02186@naver.com>
2016-09-09 13:06:08 +0900

    1932_numberTriangle problem solved

--- 1932_numberTriangle.cpp
+++ 1932_numberTriangle.cpp
@@ -3,9 +3,28 @@
 int main()
 {
 	int N;
-	scanf("%d",N);
-	int* arr = new int[N];
-	for(int i =0; i < N; i++)
-		scanf("%d",&arr[i]);
-	
+	scanf("%d",&N);
+	int** arr = new int*[N+1];
+	for(int i=0; i <= N; i++)
+		arr[i] = new int[N+1];
+	int max = 0;
+	for(int i =1; i <= N; i++)
+	{
+		for(int j =1; j <= i; j++)
+		{
+			int a;
+			scanf("%d",&a);
+			if(arr[i-1][j-1] > arr[i-1][j])
+				arr[i][j] = arr[i-1][j-1] + a;
+			else
+				arr[i][j] = arr[i-1][j] + a;
+			if(max < arr[i][j])
+				max = arr[i][j];
+		}
+	}
+
+	printf("%d",max);
+	for(int i =0;i < N+1; i++)
+		delete[] arr[i];
+	delete [] arr;
 }
