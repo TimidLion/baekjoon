07bdaf9f57c744e8634dcda17eb2331c45762b9e
TimidLion <sgm02186@naver.com>
2016-11-30 22:03:53 +0900

    1237 solved

---
+++ 2178_exploreMaze.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int maze[100][100];
+int main()
+{
+    int N,M;
+    scanf("%d %d",&N,&M);
+    for(int i=0;i < N;i++)
+        for(int j=0;j < M; j++)
+            scanf("%d",&maze[i][j]);
+
+    
+}
