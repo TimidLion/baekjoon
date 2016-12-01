4488fce31b2946b72ba27a5e88ec18803b56b6de
TimidLion <sgm02186@naver.com>
2016-12-01 16:29:59 +0900

    2156 solved

--- 2156_wineTaste.cpp
+++ 2156_wineTaste.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<algorithm>
 int wine[10001];
-int Dp[10001][2];
+int Dp[10001][3];
 using namespace std;
 int main()
 {
@@ -10,16 +10,17 @@ int main()
     for(int i=1;i <= N;i++)
         scanf("%d",&wine[i]);
 
-    Dp[0][0] = Dp[0][1] = 0;
+    Dp[0][0] = Dp[0][1] = Dp[0][2] =  0;
     Dp[1][0] = 0;
     Dp[1][1] = wine[1];
+    Dp[1][2] = wine[1];
     for(int i=2;i <= N;i++)
     {
-        Dp[i][0] = Dp[i-1][1] + wine[i];
-        Dp[i][1] = max(Dp[i-2][0]+wine[i],Dp[i-2][1]+wine[i]);
+        Dp[i][0] = max(Dp[i-1][2],Dp[i-1][1]) + wine[i];
+        Dp[i][1] = max(Dp[i-2][2],max(Dp[i-2][0],Dp[i-2][1]))+wine[i];
+        if(i != 2)
+            Dp[i][2] = max(Dp[i-3][2],max(Dp[i-3][0],Dp[i-3][0]))+wine[i];
     }
 
-    for(int i=0;i <= N; i++)
-        printf("%d: %d %d\n",i,Dp[i][0],Dp[i][1]);
     printf("%d",max(max(Dp[N][1],Dp[N][0]),max(Dp[N-1][0],Dp[N-1][1])));
 }
